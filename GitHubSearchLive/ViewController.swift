760bd4e764e40549cd6338792d6b8e343ca008df
Evgenii Bondarev <evgenii.b@softermii.com>
2025-10-27 11:53:32 +0200

    Initial Commit

---
+++ GitHubSearchLive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubSearchLive
+//
+//  Created by Evgenii Bondarev on 27.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GitHubSearchLiveTests/GitHubSearchLiveTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSearchLiveTests.swift
+//  GitHubSearchLiveTests
+//
+//  Created by Evgenii Bondarev on 27.10.2025.
+//
+
+import Testing
+@testable import GitHubSearchLive
+
+struct GitHubSearchLiveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
