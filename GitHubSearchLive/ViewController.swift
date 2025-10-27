ec5941a2dd48759413eaf52b60444de807d4e8bc
Evgenii Bondarev <evgenii.b@softermii.com>
2025-10-27 15:22:46 +0200

    Test search query

--- GitHubSearchLive/ViewController.swift
+++ GitHubSearchLive/ViewController.swift
@@ -6,12 +6,51 @@
 //
 
 import UIKit
+import Apollo
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Example: search Swift repos with lots of stars, first page
+        let searchString = "language:swift stars:>1000 sort:stars-desc"
+
+        let query = GitHubAPI.SearchRepositoriesQuery(query: searchString, first: 20, after: nil)
+        Network.shared.fetch(query: query, cachePolicy: .fetchIgnoringCacheCompletely) { result in
+          switch result {
+          case .success(let graphQLResult):
+            if let data = graphQLResult.data {
+              let connection = data.search
+              let edges = connection.edges?.compactMap { $0?.node?.asRepository }
+              let hasNext = connection.pageInfo.hasNextPage
+              let endCursor = connection.pageInfo.endCursor
+
+              // Map the data for your UI
+              let repos = edges?.map { repo in
+                return [
+                  "id": repo.id,
+                  "fullName": repo.nameWithOwner,
+                  "description": repo.description ?? "",
+                  "stars": repo.stargazerCount,
+                  "forks": repo.forkCount,
+                  "url": repo.url
+                ] as [String : Any]
+              } ?? []
+
+              print("Repos:", repos)
+              print("Has next page:", hasNext, "End cursor:", endCursor ?? "nil")
+            }
+
+            if let errors = graphQLResult.errors {
+              print("GraphQL errors:", errors)
+            }
+
+          case .failure(let error):
+            print("Network error:", error)
+          }
+        }
     }
 
 
